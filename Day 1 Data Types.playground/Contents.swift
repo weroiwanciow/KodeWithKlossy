0bfd475b8e960e5d10589bef4dcc0c00f736aefd
Apple <apple@Madj.local>
2019-07-22 12:57:35 -0500

    my first playground

---
+++ Day 1 Data Types.playground/Contents.swift
@@ -0,0 +1,49 @@
+import UIKit
+
+var str = "Hello, playground"
+str
+var name = " wero "
+name
+name = " weronika"
+print(" hello,world ")
+var age = 17.5
+var n = 7+5
+print(" I am \(age) ")
+print( "I am \(name) ")
+var first = " karlie "
+var last = " kloss "
+print ( " \(first) \(last)"  )
+print ( " I love \(first)! " )
+// 12, 65, 31 98
+var integer  = 4
+var double = 5.0
+
+var a = 12
+var b = 65
+var c = 31
+var d = 98
+
+print( " \(a + b + c + d)")
+print( " \(206 / 4 ) ")
+print(Double(a + b + c + d) / 4)
+var x = ((a + b + c + d ) / 4 )
+9 > 12
+"yes" == "no"
+6 != 9
+7 == 7
+var luckyNum = 5
+var dogAge = 5
+if dogAge < 2 {
+    print("you are a puppy") }
+else if dogAge > 12 {
+    print( "you are elderly") }
+
+var favoriteFood = "cookie dough"
+if favoriteFood == "cookie dough" {
+    print("eat more cookie dough")
+    }
+    
+else if favoriteFood == "chocolate"{
+    print("eat all the chocolate")
+    }
+
